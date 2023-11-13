a8fb26a5443ed3eb02e800e694aab122095417b7
RafaelBuiSchool <rafaelb54@nycstudents.net>
2023-11-13 00:21:19 -0500

    finished

--- Unit1-4Project/src/ShoppingCartOrganizer.java
+++ Unit1-4Project/src/ShoppingCartOrganizer.java
@@ -1,100 +1,75 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 public class ShoppingCartOrganizer {
-    public class Item {
-        private String itemName;
-        private double itemPrice;
-        private int itemQuantity;
-        private List<Item> cart = new ArrayList<>();
-        private Scanner s = new Scanner(System.in);
-        public Item(String itemName, double itemPrice, int itemQuantity) {
-            this.itemName = itemName;
-            this.itemPrice = itemPrice;
-            this.itemQuantity = itemQuantity;
-        }
-        public String toString() {
-            return "Item: " + itemQuantity + " " + itemName + "                   Price: $" + itemPrice + "\n";
-        }
-        public double getPrice(){
-            return itemPrice;
-        }
-        public String getItemName(){
-            return itemName;
-        }
-        public int getItemQuantity(){
-            return itemQuantity;
-        }
+    private static String itemName;
+    private static double itemPrice;
+    private static int itemQuantity;
+    private static List<Object> cart = new ArrayList<>();
 
-        public class choiceManager{
-            public void showMenu(String choice) {
-                if (choice.equals("0")) {
-                    ifChoiceIs0();
-                }
-                else{
-                    while (choice != "0"){
-                        if (choice == "1") {
-                            addItemToCart(itemName,itemPrice,itemQuantity);
-                        }
-                        else if(choice == "2"){
-                            removeItemFromCart(itemName);
-                        }
-                        else if(choice == "3"){
-                            viewCart();
-                        }
-                        else if (choice == "4") {
-                            generateBill();
-                        }
-                    }
+    public ShoppingCartOrganizer(String itemName, double itemPrice, int itemQuantity) {
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.itemQuantity = itemQuantity;
+    }
+    public ShoppingCartOrganizer(){}
+    public String toString() {
+        return "Item: " + itemQuantity + " " + itemName + "                   Price: $" + itemPrice + "\n";
+    }
+    public double getPrice(){
+        return itemPrice;
+    }
+    public String getItemName(){
+        return itemName;
+    }
+    public int getItemQuantity(){
+        return itemQuantity;
+    }
+    public void showMenu(String choice) {
+        if (choice.equals("0")) {
+            ifChoiceIs0();
+        } else {
+            while (choice != "0") {
+                if (choice == "1") {
+                    addItemToCart(itemName, itemPrice, itemQuantity);
+                } else if (choice == "2") {
+                    viewCart();
+                } else if (choice == "3") {
+                    generateBill();
                 }
             }
-        }
-
-
-        public String ifChoiceIs0(){
-            return ("Exiting the shopping cart organizer. Thank you!");
-        }
-        public String addItemToCart(String itemName, double itemPrice, int itemQuantity){
-            Item newItem = new Item(itemName, itemPrice, itemQuantity);
-            cart.add(newItem);
-            return("Item added sucessfully!");
-        }
-        public String removeItemFromCart(String itemName) {
-            for (Item item : cart) {
-                if (item.getItemName().equals(itemName)) {
-                    cart.remove(item);
-                    return "Item removed successfully!";
-                }
-            }
-            return "Item not found in the cart.";
-        }
-
-        public String viewCart() {
-            String cartStatus = "";
-            if (cart.isEmpty()) {
-                return("Your cart is empty.");
-            } else {
-                cartStatus = ("\nYour Cart:");
-                for (Item item : cart) {
-                    cartStatus += item.getItemName() + "\n";
-                }
-            }
-            return cartStatus;
-        }
-        public String generateBill() {
-            String individualPrice = "";
-            double total = 0;
-            for (Item item : cart) {
-                individualPrice += item.toString();
-                total += Math.abs(item.getPrice());
-            }
-            individualPrice += ("--------------------------------\n" +
-                    "Total Bill: $" + total);
-            return individualPrice;
         }
     }
+    public String ifChoiceIs0(){
+        return ("Exiting the shopping cart organizer. Thank you!");
+    }
+    public String addItemToCart(String itemName, double itemPrice, int itemQuantity){
+        ShoppingCartOrganizer newItem = new ShoppingCartOrganizer(itemName, itemPrice, itemQuantity);
+        cart.add(newItem);
+        return("Item added sucessfully!");
+    }
+    public String viewCart() {
+        String cartStatus = "";
+        if (cart.isEmpty()) {
+            return("Your cart is empty.");
+        } else {
+            cartStatus = ("\nYour Cart:");
+            for (Object item : cart) {
+                cartStatus += getItemName() + "\n";
+            }
+        }
+        return cartStatus;
+    }
+    public String generateBill() {
+        String individualPrice = "";
+        double total = 0;
+        for (Object item : cart) {
+            individualPrice += item.toString();
+            total += Math.abs(getPrice());
+        }
+        individualPrice += ("--------------------------------\n" +
+                "Total Bill: $" + total);
+        return individualPrice;
+    }
+
 }
-
-
-
-
--- Unit1-4Project/src/ShoppingListRunner.java
+++ Unit1-4Project/src/ShoppingListRunner.java
@@ -1,43 +1,36 @@
 import java.util.Scanner;
-
 public class ShoppingListRunner {
     public static void main(String[] args) {
-        ShoppingCartOrganizer shoppingCartOrganizer = new ShoppingCartOrganizer();
-        ShoppingCartOrganizer.Item choiceManager = shoppingCartOrganizer.new Item();
+        String itemName;
+        double itemPrice;
+        int itemQuantity;
+
         Scanner s = new Scanner(System.in);
-
         System.out.println("Welcome to your own personalized shopping cart! Below consists of the different options you can use to track your items...");
-
         String choice = "";
-        while (choice != "0") {
+        while (!choice.equals("0")) {
+            ShoppingCartOrganizer item = new ShoppingCartOrganizer();
             System.out.println("Shopping Cart Organizer Menu:");
             System.out.println("1. Add Item to Cart");
-            System.out.println("2. Remove Item from Cart");
-            System.out.println("3. View Cart");
-            System.out.println("4. Generate Bill");
+            System.out.println("2. View Cart");
+            System.out.println("3. Generate Bill");
             System.out.println("0. Exit");
-
             System.out.print("Enter your choice: ");
             choice = s.nextLine();
-
             if (choice.equals("1")) {
                 System.out.print("Enter item name: ");
-                String itemName = s.nextLine();
+                itemName = s.nextLine();
                 System.out.print("Enter item price: $");
-                double itemPrice = s.nextDouble();
+                itemPrice = s.nextDouble();
                 System.out.print("Enter item quantity: ");
-                int itemQuantity = s.nextInt();
-                choiceManager.addItemToCart(itemName, itemPrice, itemQuantity);
-            } else if (choice.equals("2")) {
-                System.out.print("Enter the name of the item to remove: ");
-                String itemNameToRemove = s.nextLine();
-                choiceManager.removeItemFromCart(itemNameToRemove);
-            } else if (choice.equals("3")) {
-                choiceManager.viewCart();
-            } else if (choice.equals("4")) {
-                choiceManager.generateBill();
-            } else if (choice.equals("0")) {
-                choiceManager.ifChoiceIs0();
+                itemQuantity = s.nextInt();
+                System.out.println(item.addItemToCart(itemName, itemPrice, itemQuantity));
+            } if (choice.equals("2")) {
+                System.out.println(item.viewCart());
+            } if (choice.equals("3")) {
+                System.out.println(item.generateBill());
+            } if (choice == "0") {
+                System.out.println(item.ifChoiceIs0());
             }
         }
     }
